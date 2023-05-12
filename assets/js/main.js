7cb083d6897b62b32356d51c65b4868a1d6bb80c
Martha Patricia Santos Ojeda <patysantos212@yahoo.com>
2023-05-12 15:09:04 -0500

    Primera versión de mi proyecto Carrito de Compras

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+import loader from "./components/loader.js";
+import products from "./components/products.js";
+import showMenu from "./components/showMenu.js";
+import showCart from "./components/showCart.js";
+import getProducts from "./helpers/getProducts.js";
+import cart from "./components/cart.js";
+/*UI Elements*/
+/*ocultar loader*/
+loader()
+
+/* Mostrar menu */
+showMenu ()
+
+/*Mostrar carrito*/
+showCart ()
+/* End UI Elements*/
+
+/* Products */
+const {db, printProducts} = products (await getProducts())
+
+/* Carrito*/
+cart (db, printProducts)
